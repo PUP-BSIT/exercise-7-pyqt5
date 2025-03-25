f52d5e33538f5a156dc496a997fd8c0eb55c6d54
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2025-03-25 22:31:24 +0800

    Define placeholders for items, customer details, and grand total

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
     
+    items = [] # list of items/products
+    add_item = "y" # sentinel variable for adding another item
+    grand_total = 0 # variable for the grand_total
+    
     # TODO(Caya): Implement get_order_details function
 
     # TODO(Condino): Add get customer name, and senior id input handling
