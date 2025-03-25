90d11157e80e9dca93351e22219765514fa0c1da
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2025-03-25 22:30:09 +0800

    Add TODOs for various functions to be implemented by team members

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import os
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
     
-    #TODO(Condino): Define placeholders for items, customer details, and grand total
+    # TODO(Caya): Implement get_order_details function
+
+    # TODO(Condino): Add get customer name, and senior id input handling
+
+    # TODO(Cordova): Implement calculate_grand_total function
+
+    # TODO(Arguelles): Implement display_order_summary function
+
+    # TODO(Gutierrez): Implement display_order_summary function
