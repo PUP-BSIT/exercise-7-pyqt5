30cec40f0a9f36ec84bde7061eebe496e0f7d198
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-03-25 22:27:26 +0800

    Add initial set up of main block

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+
+if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    #TODO(Condino): Define placeholders for items, customer details, and grand total
